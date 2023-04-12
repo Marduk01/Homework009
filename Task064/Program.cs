742aed6c73b2472aaefb06c32d8c87983424b67b
Igor Bodalov <igorbodalov@gmail.com>
2023-04-12 20:12:47 +0300

    Task064

---
+++ Task064/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+int i = FinalCountdown("Введите положительное число : ");
+int N = 1;
+Console.WriteLine(numbers(i, N));
+
+int numbers(int i, int N)
+{
+    if (i == N)
+        return N;
+    else
+        Console.Write($"{numbers(i, N + 1)}, ");
+    return N;
+}
+
+int FinalCountdown(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
